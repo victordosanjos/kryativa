62aaedf0b1f0056b3dd32ecac414558410c6e4c3
Victordosanjos <victoordosanjos@gmail.com>
2022-03-03 14:23:08 -0400

    Initial preparation for payment method

---
+++ kryativa-vite/src/pages/pagamento.jsx
@@ -0,0 +1,14 @@
+import react from "react";
+import { Outlet } from "react-router-dom";
+
+function Pagamentos( ) {
+    return ( 
+        <>
+        <h1>Sou lindão</h1>
+
+        <Outlet/>
+        </>
+     );
+}
+
+export default Pagamentos;
