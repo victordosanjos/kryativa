78e26eb2d193331fcf8701385f3e4d2370ac2066
Victordosanjos <victoordosanjos@gmail.com>
2022-03-03 14:21:08 -0400

    Initial preparing for payment method

--- kryativa-vite/src/configs/routes.jsx
+++ kryativa-vite/src/configs/routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Soon from '../pages/soon.jsx';
 import Triotec from '../pages/triotec.jsx';
+import Pagamentos from '../pages/pagamento.jsx';
 
 
 
@@ -10,6 +11,9 @@ export default function MainRoutes() {
         <Routes>
             <Route path="/" element={<Soon />} />
             <Route path="/clientes/triotec" element={<Triotec />} />
+            <Route path="teste" element={<Pagamentos />}>
+                <Route path="pag" element={<h1>Sei lá</h1>} />
+            </Route>
             <Route path="*" element={<h1>Não encontrado</h1>} />
 
 
