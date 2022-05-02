31ccb8253c66fc285d7664c0abe7ade691316a82
Victor dos Anjos <victoordosanjos@gmail.com>
2022-05-01 22:18:30 -0400

    Resende's virtual card done

--- kryativa-vite/src/pages/clients/drresende.jsx
+++ kryativa-vite/src/pages/clients/drresende.jsx
@@ -4,92 +4,126 @@ import logoresende from '../../images/logoresende.webp';
 import '../../design/css/Drresende.css';
 import profileicon from '../../images/clients/Dr Resende.webp';
 import MadeByUs from "../../components/MadeByUs";
-import Home from "../../design/js/mapResende";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 
 
+function Drresende() {
 
-function Drresende () {
+    const { isLoaded } = useLoadScript({
+        id: 'google-map-script',
+        googleMapsApiKey: ""
+    })
 
-    return(
+    const LocalizacaoMap = {
+        lat: -20.45769992697866,
+        lng: -54.590778831829425
+    }
+
+    return (
         <>
 
-        <div className="container-fluid d-flex align-items-center vh-100 bgGradiente">
-            <img className="BgLogo mx-auto d-block img-fluid" src={logoresende}></img>
-        </div>
-
-        <div className="container-fluid bg pagespace">
-            <div className="container-fluid bio d-flex">
-            <div className="row justify-content-around">
-                <div className="col-6 my-auto">
-                <img className="LogoBio mx-auto align-items-center" src={descricaoresende}></img>
-                </div>
-            </div>
-            <div className="col-6">
-                <img className="BioProfile rounded-circle mx-auto align-items-center d-block" src={profileicon}></img>
-
+            <div className="container-fluid d-flex align-items-center vh-100 bgGradiente">
+                <img className="BgLogo mx-auto d-block img-fluid" src={logoresende}></img>
             </div>
 
-            </div>
-        
-        <div className="container-fluid description d-flex">
-                <div className="descriptionText">
-                    <h3 className="BioText text-center">Proporcionando a harmonia entre seu sorriso e sua face.<br/><br/> Dr. Resende possui graduação em Odontologia pelo PROUNI na Uniderp. Além de, vários cursos na área e ser expert em clareamento dental. Atualmente se encontra especializando em Harmonização Orofacial no Instituto Odontológico das Américas. Com alta performance, realiza um tratamento acolhedor e humanizado.<br/><br/>•CRO/MS 7848</h3>
-                </div>
-        </div>
-        <div className="container-fluid description">
-            <br/>
-            <div className="row justify-content-around">
-                <div className="col-4 descriptionText my-auto">
-                    <h3 className="ContatoText text-center">Alguma informação personalizada</h3>
-                </div>
+            <div className="container-fluid bg pagespace">
+                <div className="container-fluid bio d-flex">
+                    <div className="row justify-content-around">
+                        <div className="col-6 my-auto">
+                            <img className="LogoBio mx-auto align-items-center" src={descricaoresende}></img>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <img className="BioProfile rounded-circle mx-auto align-items-center d-block" src={profileicon}></img>
 
-                <div className="col-4">
-                <a href="" tittle="">
-                    <button type="button" className="botao">Clique Aqui</button>
-                </a>
-                </div>
-            </div>
-        </div>
-        <div className="container-fluid text-center description">
-                <div className="row justify-content-center">
-
-                    <div className="col-4">
-                        <a href="" tittle="Instagram DrResende">
-                            <i className="bi bi-instagram socialIconD rounded-circle"></i>
-                            <h4 className="profile">Instagram</h4>
-                        </a>
                     </div>
 
-                    <div className="col-4">
-                        <a href="" tittle="Whatsapp DrResende">
-                            <i className="bi bi-whatsapp socialIconD rounded-circle"></i>
-                            <h4 className="profile">Whatsapp</h4>
-                        </a>
+                </div>
+
+                <div className="container-fluid description d-flex">
+                    <div className="container">
+                        <h3 className="BioText text-start">Proporcionando a harmonia entre seu sorriso e sua face.<br /><br /> Dr. Resende possui graduação em Odontologia pelo PROUNI na Uniderp. Além de, vários cursos na área e ser expert em clareamento dental. Atualmente se encontra especializando em Harmonização Orofacial no Instituto Odontológico das Américas. Com alta performance, realiza um tratamento acolhedor e humanizado.<br /><br />• CRO/MS 7848</h3>
                     </div>
+                </div>
+                <div className="container-fluid description">
+                    <br />
+                    <div className="row justify-content-around">
+                        <div className="col-4 descriptionText my-auto">
+                            <h3 className="ContatoText text-center">Agendar um horário</h3>
+                        </div>
 
-                    <div className="col-4">
-                        <a href="" tittle="E-mail DrResende">
-                            <i className="bi bi-envelope socialIconD rounded-circle"></i>
+                        <div className="col-4">
+                            <a href="" tittle="">
+                                <button type="btn btn-primary" className="botao">Clique Aqui</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="container-fluid text-center description">
+                    <div className="row justify-content-center">
 
-                            <h4 className="profile">E-mail</h4>
-                        </a>
+                        <div className="col-4">
+                            <a href="https://e.kryativa.dev/ISKEqY" tittle="Instagram DrResende">
+                                <i className="bi bi-instagram socialIconD rounded-circle"></i>
+                                <h4 className="profile">Instagram</h4>
+                            </a>
+                        </div>
+
+                        <div className="col-4">
+                            <a href="https://e.kryativa.dev/NEU7IG" tittle="Whatsapp DrResende">
+                                <i className="bi bi-whatsapp socialIconD rounded-circle"></i>
+                                <h4 className="profile">Whatsapp</h4>
+                            </a>
+                        </div>
+
+                        <div className="col-4">
+                            <a href="mailto:contato@drresende.com" tittle="E-mail DrResende">
+                                <i className="bi bi-envelope socialIconD rounded-circle"></i>
+
+                                <h4 className="profile">E-mail</h4>
+                            </a>
+                            <br />
+                            <br />
+                        </div>
+
+
+                    </div>
+                </div>
+                <div className="container adress text-center">
+                    <a href="https://goo.gl/maps/HCCqcDJQPYTxs9TVA">
+                    <h4 className="BioText">Visite meu consultório
                         <br />
-                        <br />
-                    </div>
-
-        
+                        R. João Akamine, 103 - Santa Fe, Campo Grande - MS, 79021-240</h4>
+                    </a>
                 </div>
+
+                <div className="container-fluid mapControl">
+                    {
+
+                        isLoaded ? (
+                            <GoogleMap
+                                mapContainerStyle={{ width: '100%', height: '100%' }}
+                                center={LocalizacaoMap}
+                                zoom={15}
+                            >
+                                <Marker position={LocalizacaoMap} />
+                            </GoogleMap>
+                        ) : <></>
+                    }
+                </div>
+
+                <br />
+                <div className="text-center">
+                    <br/>
+                    <MadeByUs />
+                    <br/>
+                </div>
+
+
             </div>
-        <div className="container adress text-center">
-            <h4 className="BioText">VISITE MEU CONSULTÓRIO<br/>TRAVESSA ILHA BELA, 264 - CAMPO GRANDE, MS</h4>
-            <br/>
-            <MadeByUs/>
-        </div>
 
-    </div>
 
-    
-    
+
         </>
     )
 
